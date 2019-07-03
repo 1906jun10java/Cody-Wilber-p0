cf7cc925f206bee8d065a0dd9b6c6239c3a9de05
Cody Johnson <codyj@protonmail.com>
2019-07-03 10:25:26 -0400

    Update Driver

--- src/main/java/com/revature/driver/Driver.java
+++ src/main/java/com/revature/driver/Driver.java
@@ -1,13 +1,27 @@
 package com.revature.driver;
 
+import com.revature.services.CarService;
+import com.revature.services.CustomerService;
+import com.revature.services.EmployeeService;
+import com.revature.services.OfferService;
 import com.revature.views.user.UserHomeMenu;
 
-public class Driver {
+public class Driver {	
 	public static void main(String[] args) {
 		// Initialize services
+		CustomerService customerService = CustomerService.getInstance();
+		EmployeeService employeeService = EmployeeService.getInstance();
+		CarService carService = CarService.getInstance();
+		OfferService offerService = OfferService.getInstance();
+		
+		// Load data
+		customerService.loadCustomers();
+		employeeService.loadEmployees();
+		carService.loadCars();
+		offerService.loadOffers();
 		
 		// Display home menu
 		UserHomeMenu uhm = new UserHomeMenu();
-		uhm.getResponse(uhm.display());
+		uhm.redirect(uhm.display());
 	}
 }
