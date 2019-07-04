2d19cdaa6d393576488c6acf0197dcd062659a5b
Cody Johnson <codyj@protonmail.com>
2019-07-03 22:11:19 -0400

    Clean up

--- src/main/java/com/revature/views/customer/CustomerCarListView.java
+++ src/main/java/com/revature/views/customer/CustomerCarListView.java
@@ -1,5 +1,6 @@
 package com.revature.views.customer;
 
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 import com.revature.beans.Car;
@@ -24,8 +25,10 @@ public class CustomerCarListView {
 			System.out.println("Make:\t\t" + c.getMake());
 			System.out.println("Model:\t\t" + c.getModel());
 			System.out.println("Mileage:\t" + c.getMileage());
-			System.out.println("Price:\t\t$" + c.getPrice().setScale(2));
-			System.out.println("Balance:\t$" + c.getBalance().setScale(2));
+			System.out.println("Price:\t\t$" +
+				c.getPrice().setScale(2, RoundingMode.HALF_UP));
+			System.out.println("Balance:\t$" +
+				c.getBalance().setScale(2, RoundingMode.HALF_UP));
 			System.out.println();
 		}
 	}
