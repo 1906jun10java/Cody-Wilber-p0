77afef4deef8bb9f86ba9e78e24a7dbf64385e5a
Cody Johnson <codyj@protonmail.com>
2019-07-04 20:39:34 -0400

    Implement EmployeeDAO and update CustomerDAO

--- src/main/java/com/revature/dao/CustomerDAO.java
+++ src/main/java/com/revature/dao/CustomerDAO.java
@@ -3,7 +3,6 @@ package com.revature.dao;
 import com.revature.beans.Customer;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public interface CustomerDAO {
     void saveCustomer(Customer c) throws SQLException;
--- src/main/java/com/revature/dao/CustomerDAOImpl.java
+++ src/main/java/com/revature/dao/CustomerDAOImpl.java
@@ -4,7 +4,6 @@ import com.revature.beans.Customer;
 import com.revature.services.ConnectionService;
 
 import java.sql.*;
-import java.util.ArrayList;
 
 public class CustomerDAOImpl implements CustomerDAO {
     private static ConnectionService cs = ConnectionService.getInstance();
