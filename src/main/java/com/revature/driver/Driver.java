26f436e44d7dd7667c8d6ba7b2fe7e0e562828d7
Cody Johnson <codyj@protonmail.com>
2019-07-04 20:51:18 -0400

    Refactor Driver

--- src/main/java/com/revature/driver/Driver.java
+++ src/main/java/com/revature/driver/Driver.java
@@ -1,25 +1,9 @@
 package com.revature.driver;
 
-import com.revature.services.*;
 import com.revature.views.user.UserHomeMenu;
 
 public class Driver {	
 	public static void main(String[] args) {
-		// Initialize services
-		//CustomerService customerService = CustomerService.getInstance();
-		//EmployeeService employeeService = EmployeeService.getInstance();
-		//CarService carService = CarService.getInstance();
-		//OfferService offerService = OfferService.getInstance();
-		//PaymentService paymentService = PaymentService.getInstance();
-		
-		// Load data
-		//customerService.loadCustomers();
-		//employeeService.loadEmployees();
-		//carService.loadCars();
-		//offerService.loadOffers();
-		//paymentService.loadPayments();
-		
-		// Display home menu
 		UserHomeMenu uhm = new UserHomeMenu();
 		uhm.redirect(uhm.display());
 	}
